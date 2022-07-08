c48d23a2a67325a6569052ab2821e1597dfb20b1
zhy2on <dhwlgus5940@gmail.com>
2022-07-08 23:02:16 +0900

    Test: practice ray tracing using vec3 class - gradient, sphere

---
+++ Day1/sphere/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "sdltemplate.h"
+#include "ray.h"
+
+bool hit_sphere(const vec3& center, double radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b * b - 4 * a * c;
+  return (discriminant > 0);
+}
+
+vec3 color(const ray& r) {
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+		return (vec3(1, 0, 0));
+	vec3 unit_dir = unit_vector(r.direction());
+	float t = 0.5*(unit_dir.y() + 1.0);
+	return (1.0-t)*vec3(1.0,1.0,1.0)+t*vec3(0.5,0.7,1.0);
+}
+
+int main() {
+	int width = 800;
+	int height = 400;
+	std::cout << "P3\n" << width << " " << height << "\n255\n";
+	sdltemplate::sdl("Ray Tracer", width, height);
+	sdltemplate::loop();
+	vec3 lower_left_corner(-2.0,-1.0,-1.0);
+	vec3 horizontal(4.0,0.0,0.0);
+	vec3 vertical(0.0,2.0,0.0);
+	vec3 origin(0.0,0.0,0.0);
+	for(int y=height-1; y>=00; y--) {
+		for(int x=0; x<width; x++) {
+			float u = float(x) / float(width);
+			float v = float(y) / float(height);
+			ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+			vec3 col = color(r);
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
+			std::cout << ir << " " << ig << " " << ib << "\n";
+			sdltemplate::setDrawColor(sdltemplate::createColor(ir,ig,ib,255));
+			sdltemplate::drawPoint(x,height-y);
+		}
+	}
+	while(sdltemplate::running) {
+		sdltemplate::loop();
+	}
+}
