a2d6bdf3346c67ced3deb8bd2312915ff2a28bcb
zhy2on <dhwlgus5940@gmail.com>
2022-07-08 23:28:30 +0900

    Test: basic shadowing

---
+++ Day2/sphere-shadow/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "sdltemplate.h"
+#include "ray.h"
+
+double hit_sphere(const vec3& center, double radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b * b - 4 * a * c;
+
+  if (discriminant < 0)
+  {
+	  return -1.0;
+  }
+  else
+  {
+	  return (-b - sqrt(discriminant)) / (2.0 * a);
+  }
+}
+
+vec3 ray_color(const ray& r) {
+	auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+	vec3 unit_direction = unit_vector(r.direction());
+    t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
+int main() {
+	int width = 800;
+	int height = 400;
+	std::cout << "P3\n" << width << " " << height << "\n255\n";
+	sdltemplate::sdl("Ray Tracer", width, height);
+	sdltemplate::loop();
+	vec3 lower_left_corner(-2.0,-1.0,-1.0);
+	vec3 horizontal(4.0,0.0,0.0);
+	vec3 vertical(0.0,2.0,0.0);
+	vec3 origin(0.0,0.0,0.0);
+	for(int y=height-1; y>=00; y--) {
+		for(int x=0; x<width; x++) {
+			float u = float(x) / float(width);
+			float v = float(y) / float(height);
+			ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+			vec3 col = ray_color(r);
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
+			std::cout << ir << " " << ig << " " << ib << "\n";
+			sdltemplate::setDrawColor(sdltemplate::createColor(ir,ig,ib,255));
+			sdltemplate::drawPoint(x,height-y);
+		}
+	}
+	while(sdltemplate::running) {
+		sdltemplate::loop();
+	}
+}
