fb7a07b22cde78a078d37b66f79a7cdc25e23408
zhy2on <dhwlgus5940@gmail.com>
2022-07-11 03:49:56 +0900

    Feat: color.h, rt_weekend.h 추가

--- incs/sphere.h
+++ incs/sphere.h
@@ -1,56 +1,51 @@
-#ifndef SPHEREH
-#define SPHEREH
-//==================================================================================================
-// Written in 2016 by Peter Shirley <ptrshrl@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all copyright and related and
-// neighboring rights to this software to the public domain worldwide. This software is distributed
-// without any warranty.
-//
-// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication along
-// with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//==================================================================================================
+#ifndef SPHERE_H
+#define SPHERE_H
 
-#include "hitable.h"
+#include "hittable.h"
+#include "vec3.h"
 
+class sphere : public hittable {
+public:
+    sphere() { }
+    sphere(point3 cen, double r)
+        : center(cen)
+        , radius(r) {};
 
-class sphere: public hitable  {
-    public:
-        sphere() {}
-        sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m)  {};
-        virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
-        vec3 center;
-        float radius;
-        material *mat_ptr;
+    virtual bool hit(
+        const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+public:
+    point3 center;
+    double radius;
 };
 
-
-bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const
+{
     vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = dot(oc, r.direction());
-    float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - a*c;
-    if (discriminant > 0) {
-        float temp = (-b - sqrt(discriminant))/a;
-        if (temp < t_max && temp > t_min) {
-            rec.t = temp;
-            rec.p = r.point_at_parameter(rec.t);
-            rec.normal = (rec.p - center) / radius;
-            rec.mat_ptr = mat_ptr;
-            return true;
-        }
-        temp = (-b + sqrt(discriminant)) / a;
-        if (temp < t_max && temp > t_min) {
-            rec.t = temp;
-            rec.p = r.point_at_parameter(rec.t);
-            rec.normal = (rec.p - center) / radius;
-            rec.mat_ptr = mat_ptr;
-            return true;
-        }
-    }
-    return false;
-}
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
 
+    auto discriminant = half_b * half_b - a * c;
+    if (discriminant < 0)
+        return false;
+    auto sqrtd = sqrt(discriminant);
+
+    // Find the nearest root that lies in the acceptable range.
+    auto root = (-half_b - sqrtd) / a;
+    if (root < t_min || t_max < root) {
+        root = (-half_b + sqrtd) / a;
+        if (root < t_min || t_max < root)
+            return false;
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    // rec.normal = (rec.p - center) / radius;
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
+
+    return true;
+}
 
 #endif
