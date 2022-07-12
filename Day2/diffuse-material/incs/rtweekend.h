e6534dd508f2a8bf272be57878a68940b4fce83b
zhy2on <dhwlgus5940@gmail.com>
2022-07-13 03:14:23 +0900

    Feat: gamma correction

--- Day2/diffuse-material/incs/rtweekend.h
+++ Day2/diffuse-material/incs/rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // Usings
 
@@ -27,5 +28,21 @@ inline double degrees_to_radians(double degrees)
 
 #include "ray.h"
 #include "vec3.h"
+
+// inline double random_double() {
+//     // Returns a random real in [0,1).
+//     return rand() / (RAND_MAX + 1.0);
+// }
+
+// inline double random_double(double min, double max) {
+//     // Returns a random real in [min,max).
+//     return min + (max-min)*random_double();
+// }
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
 
 #endif
