8091380f060e3fd7bb7f57e3d06e595db3cd704f
zhy2on <dhwlgus5940@gmail.com>
2022-07-13 02:23:17 +0900

    Fix: antialiasing

--- Day2/diffuse/main.cpp
+++ Day2/diffuse/main.cpp
@@ -5,7 +5,6 @@
 #include "rtweekend.h"
 #include "sdltemplate.h"
 #include "sphere.h"
-#include "camera.h"
 
 color ray_color(const ray& r, const hittable& world)
 {
@@ -25,7 +24,6 @@ int main()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 800;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -33,7 +31,15 @@ int main()
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
-    camera cam;
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     // Render
     std::cout << "P3\n"
@@ -43,17 +49,17 @@ int main()
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width - 1);
-                auto v = (j + random_double()) / (image_height - 1);
-                ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
-            }
-            write_color(std::cout, pixel_color, samples_per_pixel);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            color pixel_color = ray_color(r, world);
+            write_color(std::cout, pixel_color);
 
-            pixel_color = get_color(pixel_color, samples_per_pixel);
-            sdltemplate::setDrawColor(sdltemplate::createColor(pixel_color.x(), pixel_color.y(), pixel_color.z(), 255));
+            double ir = (255.999 * pixel_color.x());
+            double ig = (255.999 * pixel_color.y());
+            double ib = (255.999 * pixel_color.z());
+
+            sdltemplate::setDrawColor(sdltemplate::createColor(ir, ig, ib, 255));
             sdltemplate::drawPoint(i, image_height - j);
         }
     }
